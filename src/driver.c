cf6789859540e772c166a927ecba3b751fd8f718
nashs789 <bbock1214@gmail.com>
2023-09-04 19:41:49 +0000

    [ Upt - LIB ] Makefile 플래그 수정 및 함수 추가

--- src/driver.c
+++ src/driver.c
@@ -1,15 +1,20 @@
-#include "rbtree.h"
+#include "rbtree.c"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    rbtree *R = (rbtree*) malloc(sizeof(rbtree));
-    int num[] = {1, 5, 10, 8, 32, 200, 123, 4123, 5345, 6777, 788, 2, 0, 2};
+    rbtree *tree = new_rbtree();
+    int num[] = {20, 10, 50, 30, 80, 40, 35, 25};
 
-    for (int i = 13; i >= 0; i--) {
-        printf("\n");
-        rbtree_insert(R, num[i]);
+    for (int i = 0; i < 8; i++) {
+        rbtree_insert(tree, num[i]);
     }
 
-    show_tree(R);
+    show_tree(tree, tree -> root);
+    rbtree_find(tree, 6);
+    rbtree_find(tree, 11);
+    rbtree_find(tree, 1);
+
+    rbtree_min(tree);
+    rbtree_max(tree);
 }
